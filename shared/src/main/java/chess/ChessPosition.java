3d19a5cc3c5654d3437ebdd472e77af1d9757660
Trevor Larsen <trevorlarsen357@gmail.com>
2024-09-11 23:09:59 -0600

    Implemented hashcode for ChessPosition

--- shared/src/main/java/chess/ChessPosition.java
+++ shared/src/main/java/chess/ChessPosition.java
@@ -1,5 +1,7 @@
 package chess;
 
+import java.util.Objects;
+
 /**
  * Represents a single square position on a chess board
  * <p>
@@ -38,6 +40,11 @@ public class ChessPosition {
         if (o == null || getClass() != o.getClass()) return false;
         ChessPosition that = (ChessPosition) o;
         return row == that.getRow() && col == that.getColumn();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(row, col);
     }
 
     @Override
