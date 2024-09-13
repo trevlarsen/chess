ac18a608f0f477d16a0130e2f68b25ac59ac2430
Trevor Larsen <trevorlarsen357@gmail.com>
2024-09-12 21:06:04 -0600

    Implemented PawnMoves, passed tests

--- shared/src/main/java/chess/PawnMoves.java
+++ shared/src/main/java/chess/PawnMoves.java
@@ -9,13 +9,12 @@ public class PawnMoves {
 
     /*
     Accepts ChessBoard, ChessPosition, and ChessPiece
-    Compute all possible Queen moves, disregarding the King's status
+    Compute all possible Pawn moves, disregarding the King's status
      */
     public static ArrayList<ChessMove> calculateMoves(ChessBoard board, ChessPosition position, ChessPiece piece) {
 
-        // Initialize list of moves
         var moves = new ArrayList<ChessMove>();
-
+        // Determine direction of movement based on piece color
         int scale;
         if (piece.getTeamColor() == ChessGame.TeamColor.WHITE) {
             scale = 1;
@@ -23,9 +22,66 @@ public class PawnMoves {
             scale = -1;
         }
 
-        // Basic Move
-        if (ChessPiece.inBounds(position.getRow() + scale, position.getColumn())) {
-            scale = 1;
+        // Forward move direction
+        var row = position.getRow() + scale;
+        var col = position.getColumn();
+        ChessPiece current = board.pieces[row-1][col-1];
+        if (current == null) {
+            moves.add(new ChessMove(position, new ChessPosition(row, col), null));
+        }
+
+        // Capture move directions
+        int[] xs = {scale, scale};
+        int[] ys = {1, -1};
+        for (int i = 0; i < 2; i++) {
+            var row2 = position.getRow() + xs[i];
+            var col2 = position.getColumn() + ys[i];
+
+            if (ChessPiece.inBounds(row, col)) {
+                ChessPiece current2 = board.pieces[row2 - 1][col2 - 1];
+
+                if (current2 != null && current2.getTeamColor() != piece.getTeamColor()) {
+                    moves.add(new ChessMove(position, new ChessPosition(row2, col2), null));
+                }
+            }
+        }
+
+        // Add promotion options for eligible moves
+        var toRemove = new ArrayList<ChessMove>();
+        var toAdd = new ArrayList<ChessMove>();
+        for (var move : moves) {
+            // Determine if move puts piece in first or last row
+            var r = move.getEndPosition().getRow();
+            if (r == 1 || r == 8 ) {
+                // Extract old move information
+                var start = move.getStartPosition();
+                var end = move.getEndPosition();
+
+                // Schedule to remove old move
+                toRemove.add(move);
+
+                // Add new moves with promotion options
+                toAdd.add(new ChessMove(start, end, ChessPiece.PieceType.QUEEN));
+                toAdd.add(new ChessMove(start, end, ChessPiece.PieceType.BISHOP));
+                toAdd.add(new ChessMove(start, end, ChessPiece.PieceType.KNIGHT));
+                toAdd.add(new ChessMove(start, end, ChessPiece.PieceType.ROOK));
+            }
+        }
+        moves.removeAll(toRemove);
+        moves.addAll(toAdd);
+
+        // First move direction
+        if ((position.getRow() == 2 && piece.getTeamColor() == ChessGame.TeamColor.WHITE) || (position.getRow() == 7 && piece.getTeamColor() == ChessGame.TeamColor.BLACK)) {
+            var row3 = position.getRow() + scale;
+            var col3 = position.getColumn();
+            ChessPiece current3 = board.pieces[row3 - 1][col3 - 1];
+            if (current3 == null) {
+                var row4 = row3 + scale;
+                ChessPiece current4 = board.pieces[row4 - 1][col3 - 1];
+                if (current4 == null) {
+                    moves.add(new ChessMove(position, new ChessPosition(row4, col3), null));
+                }
+            }
         }
 
         return moves;
