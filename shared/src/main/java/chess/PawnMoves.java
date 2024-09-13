eade7b7167fb3ce0a31765bfd48b00f0db8291e5
Trevor Larsen <trevorlarsen357@gmail.com>
2024-09-12 21:29:28 -0600

    Fixed PawnMoves, passed Phase0 tests

--- shared/src/main/java/chess/PawnMoves.java
+++ shared/src/main/java/chess/PawnMoves.java
@@ -25,6 +25,7 @@ public class PawnMoves {
         // Forward move direction
         var row = position.getRow() + scale;
         var col = position.getColumn();
+        // Check if spot is open
         ChessPiece current = board.pieces[row-1][col-1];
         if (current == null) {
             moves.add(new ChessMove(position, new ChessPosition(row, col), null));
@@ -33,13 +34,14 @@ public class PawnMoves {
         // Capture move directions
         int[] xs = {scale, scale};
         int[] ys = {1, -1};
+        // For each capture position
         for (int i = 0; i < 2; i++) {
             var row2 = position.getRow() + xs[i];
             var col2 = position.getColumn() + ys[i];
-
-            if (ChessPiece.inBounds(row, col)) {
+            // Check bounds
+            if (ChessPiece.inBounds(row2, col2)) {
                 ChessPiece current2 = board.pieces[row2 - 1][col2 - 1];
-
+                // Check if capture is available
                 if (current2 != null && current2.getTeamColor() != piece.getTeamColor()) {
                     moves.add(new ChessMove(position, new ChessPosition(row2, col2), null));
                 }
@@ -60,22 +62,25 @@ public class PawnMoves {
                 // Schedule to remove old move
                 toRemove.add(move);
 
-                // Add new moves with promotion options
+                // Schedule to add new moves with promotion options
                 toAdd.add(new ChessMove(start, end, ChessPiece.PieceType.QUEEN));
                 toAdd.add(new ChessMove(start, end, ChessPiece.PieceType.BISHOP));
                 toAdd.add(new ChessMove(start, end, ChessPiece.PieceType.KNIGHT));
                 toAdd.add(new ChessMove(start, end, ChessPiece.PieceType.ROOK));
             }
         }
+        // Update list with promotion information
         moves.removeAll(toRemove);
         moves.addAll(toAdd);
 
         // First move direction
         if ((position.getRow() == 2 && piece.getTeamColor() == ChessGame.TeamColor.WHITE) || (position.getRow() == 7 && piece.getTeamColor() == ChessGame.TeamColor.BLACK)) {
+            // Check if one spot ahead is open
             var row3 = position.getRow() + scale;
             var col3 = position.getColumn();
             ChessPiece current3 = board.pieces[row3 - 1][col3 - 1];
             if (current3 == null) {
+                // Check if two spots ahead is open
                 var row4 = row3 + scale;
                 ChessPiece current4 = board.pieces[row4 - 1][col3 - 1];
                 if (current4 == null) {
