30bf1a346a8439f980e406608eb83a0a0dce2906
Trevor Larsen <trevorlarsen357@gmail.com>
2024-11-05 23:04:24 -0700

    small fix to menus

--- client/src/main/java/ui/PostloginMenu.java
+++ client/src/main/java/ui/PostloginMenu.java
@@ -3,7 +3,13 @@ package ui;
 public class PostloginMenu {
 
     private final String postloginMenu = """
-            Gameplay not implemented yet.
+            You are not logged in. What would you like to do?
+            \t1. Logout
+            \t2. List Games
+            \t3. Create Game
+            \t4. Play Game
+            \t5. Observe Game
+            \t6. Help
             """;
 
     public MenuState run() {
